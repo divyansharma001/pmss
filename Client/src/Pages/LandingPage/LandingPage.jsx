817d089a6db5fc3a2ffb6bca4d88e10d8927d7a7
Riya Sharma <sharmariya11133@gmail.com>
2024-09-28 13:35:31 +0530

    new file added

--- Client/src/Pages/LandingPage/LandingPage.jsx
+++ Client/src/Pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import "./LandingPage.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./LandingPage.css"; // Assuming you already have the necessary styles
+import heroImg from "../../../public/Hero-img.jpg";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      {/* Hero Section */}
+      {/* Hero Section with Carousel */}
       <header className="hero">
-        <div className="hero-content">
-          <h1>Empowering Education through PMSSS</h1>
-          <p>Providing scholarships to deserving students across India.</p>
-          <button className="cta-button">Apply Now</button>
-          <button className="cta-button secondary">Learn More</button>
-        </div>
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showThumbs={false}
+          showStatus={false}
+          interval={5000}
+        >
+          {/* Slide 1 */}
+          <div className="carousel-slide">
+            <img src={heroImg} alt="Scholarship Banner" />{" "}
+            {/* Update the image path */}
+          </div>
+          {/* Slide 2 */}
+          <div className="carousel-slide">
+            <img src={heroImg} alt="Scholarship Banner" />
+          </div>
+          {/* Slide 3 */}
+          <div className="carousel-slide">
+            <img src={heroImg} alt="Scholarship Banner" />
+          </div>
+          {/* Slide 4 */}
+          <div className="carousel-slide">
+            <img src={heroImg} alt="Scholarship Banner" />
+          </div>
+        </Carousel>
       </header>
 
       {/* Scrolling Ticker */}
